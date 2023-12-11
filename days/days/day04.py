b77bb2482dec15d327cbfcabf6db25edf8dffef3
Dr. Raycism <narutoprogamerxd@gmail.com>
2023-12-11 14:35:22 +0100

    Day 4 - Done

--- days/days/day04.py
+++ days/days/day04.py
@@ -1,9 +1,5 @@
-# Open the file called "input.txt" in the same directory as this file.
-input_file = open("input.txt", 'r')
-# Read the file into a list where every element is a line from the file in order.
-# The lines are stored as strings.
+input_file = open("input/day04.txt", 'r')
 input = input_file.read().strip().split("\n")
-# Close the file before we start solving.
 input_file.close()
 
 winning_nums, owned_nums = [],[] 
