6ada573f65571bbfdd74fe091b90baac5ef4023b
Dr. Raycism <narutoprogamerxd@gmail.com>
2023-12-13 12:51:53 +0100

    Cleaned

--- days/days/day05_2.py
+++ days/days/day05_2.py
@@ -50,7 +50,6 @@ print('Day 05 Part 1:',min(locs))
 print('Day 05 Part 1:',(v:=[[list(map(int,l.split())) for l in d.split('\n')] for d in [d for _,d in [re.split(r':\n',s) for s in re.split(r'\n\n',q[5].strip())[1:]]]]) and (c:=lambda i,m:(lambda c:c[0]+(i-c[1]) if c[1]+c[2]>i else i)(min([y for y in m if y[1]<=i],default=[0,0,0],key=lambda x:i-x[1]))) and min([c(s,v[6])for s in [c(s,v[5]) for s in [c(s,v[4]) for s in [c(s,v[3]) for s in [c(s,v[2]) for s in [c(s,v[1]) for s in [c(s,v[0]) for s in list(map(int,re.split(r'\n\n',q[5].strip())[0].split()[1:]))]]]]]]]))
 
 
-######################### PART 2: MULTI-LINE SOLUTION #########################
 sections = re.split(r'\n\n', q[5].strip())
 seeds = list(map(int, sections[0].split()[1:]))
 maps = sections[1:]
